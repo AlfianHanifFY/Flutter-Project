0e40fd6db1e066e7eba044029841deeba5b63036
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-03-02 11:05:13 +0700

    update login screen

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -14,14 +15,47 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          color: Colors.blue,
-          width: 400,
-          height: 300,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [Text('halo')],
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Container(
+            color: const Color.fromARGB(255, 155, 203, 243),
+            width: 400,
+            height: 450,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Login'),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    labelText: 'Username',
+                    prefixIcon: Icon(Icons.people_alt_rounded),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                  obscureText: true,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Perform action on button press
+                  },
+                  child: Text('Login'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        const Color.fromARGB(255, 216, 234, 249)),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
